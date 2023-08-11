116b980b434677f7a998ab45ded1494e79e2d812
Yvo （月影） <akira.cn@gmail.com>
2023-08-11 13:11:17 +0800

    Update via AirCode on Aug 11, 2023

--- examples/modular-kv-juejin/storage.mjs
+++ examples/modular-kv-juejin/storage.mjs
@@ -1,11 +1,11 @@
 import aircode from 'aircode';
-import {config, modular} from 'http-modular';
+import {config, context, modular} from 'http-modular';
 import {sha256 as hash} from 'crypto-hash';
 
-function auth(context) {
-  const origin = context.headers.origin;
-  const referer = context.headers.referer;
-  const xBucketId = context.headers['x-bucket-id'];
+const auth = context(async (ctx) => {
+  const origin = ctx.headers.origin;
+  const referer = ctx.headers.referer;
+  const xBucketId = ctx.headers['x-bucket-id'];
 
   if(origin !== 'https://code.devrank.cn') {
     throw new Error(JSON.stringify({error: {reason: '非法访问'}}));
@@ -15,26 +15,26 @@ function auth(context) {
     throw new Error(JSON.stringify({error: {reason: '缺少projectId，需要在HTML中添加<meta name="referrer" content="no-referrer-when-downgrade"/>，Safari浏览器请取“消阻止跨站跟踪选项”。'}}));
   }
 
-  const bucket = hash(xBucketId || (referer && referer.split('?projectId=')[1]));
+  const bucket = await hash(xBucketId || (referer && referer.split('?projectId=')[1]));
   return aircode.db.table(`storage-${bucket}`)
-}
+});
 
-async function setItem(key, value, context) {
-  return await auth(context).where({key}).set({value}).upsert(true).save();
-}
+const setItem = context(auth, async (table, key, value) => {
+  return await table.where({key}).set({value}).upsert(true).save();
+});
 
-async function getItem(key, context) {
-  const res = await auth(context).where({key}).findOne();
+const getItem = context(auth, async (table, key) => {
+  const res = await table.where({key}).findOne();
   return res?.value;
-}
+});
 
-async function removeItem(key, context) {
-  return await auth(context).where({key}).delete();
-}
+const removeItem = context(auth, async (table, key) => {
+  return await table.where({key}).delete();
+});
 
-async function clear(context) {
-  return await auth(context).drop();
-}
+const clear = context(auth, async (table) => {
+  return await table.drop();
+});
 
 export default modular({
   setItem,
